08942cd1032f5b9ae20d93d00b2f449075634699
Nattasak <dottedes@gmail.com>
2018-01-24 16:47:23 +0700

    style: change class component to functional

--- src/components/SnippetShow/SnippetShow.js
+++ src/components/SnippetShow/SnippetShow.js
@@ -2,36 +2,12 @@ import React, { Component, Fragment } from 'react';
 
 import './SnippetShow.css';
 
-// const SnippetShow = ({ data }) => (
-//   <Fragment>
-//     <h1 className="title">{data.lang}</h1>
-//     <h2 className="subtitle">{data.title}</h2>
-//     <div className="content">{data.code}</div>
-//   </Fragment>
-// );
-
-class SnippetShow extends Component {
-  render() {
-    const { data } = this.props;
-
-    // console.log(this.props.data);
-
-    return (
-      <Fragment>
-        <h1 className="title">{data.lang}</h1>
-        <h2 className="subtitle">{data.title}</h2>
-        <div className="content">{data.code}</div>
-      </Fragment>
-    );
-  }
-}
-
-SnippetShow.defaultProps = {
-  data: {
-    lang: 'JavaScript',
-    title: 'Array Concat',
-    code: '[1, 2, 3, 4]',
-  },
-};
+const SnippetShow = ({ data }) => (
+  <Fragment>
+    <h1 className="title">{data.lang}</h1>
+    <h2 className="subtitle">{data.title}</h2>
+    <div className="content">{data.code}</div>
+  </Fragment>
+);
 
 export default SnippetShow;
